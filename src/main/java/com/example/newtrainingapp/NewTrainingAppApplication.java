ce200141384bf69a7f80373d2c3a13ea21bbb466
Przemek <przemyslawwlodarczyk90@gmail.com>
2024-04-24 19:27:26 +0200

    Initial commit

---
+++ src/main/java/com/example/newtrainingapp/NewTrainingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newtrainingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTrainingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewTrainingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newtrainingapp/NewTrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newtrainingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTrainingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
